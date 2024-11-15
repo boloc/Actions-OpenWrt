25e22a7377966373f869d4c3d45ab9369e2b0f1b
ZhouZepeng <515709074@qq.com>
2024-11-16 03:04:32 +0800

    edit config description

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,8 @@
 
 # 2024.11.16 agron主题不适配23.05,以及可能存在部分其他不兼容的所以取以前18.06的
 sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05$/s/^/#/' feeds.conf.default
+
+# 引入别人的插件集合
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
