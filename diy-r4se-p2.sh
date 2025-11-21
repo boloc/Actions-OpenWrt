429cb8a63fe832c9b6d9a09385fc87607574ae3d
ZhouZepeng <515709074@qq.com>
2025-11-21 20:01:03 +0800

    fix

--- diy-r4se-p2.sh
+++ diy-r4se-p2.sh
@@ -6,8 +6,8 @@
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile 2>/dev/null || true
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile 2>/dev/null || true
 
-# 修改默认 IP 地址为 192.168.2.60
-sed -i 's/192.168.1.1/192.168.2.60/g' package/base-files/files/bin/config_generate 2>/dev/null || true
+# 修改默认 IP 地址为 192.168.10.1
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 2>/dev/null || true
 
 
 
