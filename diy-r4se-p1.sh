eef4f799f2fb865100d98aa094b003b21c99ce6a
ZhouZepeng <515709074@qq.com>
2025-11-24 11:50:07 +0800

    fix

--- diy-r4se-p1.sh
+++ diy-r4se-p1.sh
@@ -3,17 +3,13 @@
 # R4SE (master) - feeds 定制
 
 # 追加 passwall 与 AdGuardHome 的第三方源（避免重复追加）
-if ! grep -q 'passwall_packages' feeds.conf.default; then
-  echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-fi
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
 
-if ! grep -q 'passwall_luci' feeds.conf.default; then
-  echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
-fi
-
-if ! grep -q 'luci_adguardhome' feeds.conf.default; then
-  echo 'src-git luci_adguardhome https://github.com/sirpdboy/luci-app-adguardhome.git' >> feeds.conf.default
-fi
+# # 追加 AdGuardHome 的第三方源
+# if ! grep -q 'luci_adguardhome' feeds.conf.default; then
+#   echo 'src-git luci_adguardhome https://github.com/sirpdboy/luci-app-adguardhome.git' >> feeds.conf.default
+# fi
 
 # 删除 ImmortalWrt 自带的 fullconenat-nft，避免与 TurboACC 冲突
 rm -rf package/network/utils/fullconenat-nft/ || true
