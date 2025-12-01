eb05051ea239785342d7103587d2229f800a39ab
ZhouZepeng <515709074@qq.com>
2025-12-01 15:56:09 +0800

    不编译smartdns

--- diy-r4se-p1.sh
+++ diy-r4se-p1.sh
@@ -2,14 +2,12 @@
 
 # R4SE (master) - feeds 定制
 
-# 追加 passwall 与 AdGuardHome 的第三方源（避免重复追加）
+# 追加 passwall
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
 
 # # 追加 AdGuardHome 的第三方源
-# if ! grep -q 'luci_adguardhome' feeds.conf.default; then
-#   echo 'src-git luci_adguardhome https://github.com/sirpdboy/luci-app-adguardhome.git' >> feeds.conf.default
-# fi
+# echo 'src-git luci_adguardhome https://github.com/sirpdboy/luci-app-adguardhome.git' >> feeds.conf.default
 
 # 删除 ImmortalWrt 自带的 fullconenat-nft，避免与 TurboACC 冲突
 rm -rf package/network/utils/fullconenat-nft/ || true
