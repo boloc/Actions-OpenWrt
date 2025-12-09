399cf7f57703f5cd11f06f8b2694b916ed8ffd69
ZhouZepeng <515709074@qq.com>
2025-12-09 14:02:44 +0800

    fix

--- diy-r4se-p1.sh
+++ diy-r4se-p1.sh
@@ -2,9 +2,9 @@
 
 # R4SE (master) - feeds 定制
 
-# 追加 passwall
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+# 在顶部插入 passwall (先执行的会在下面，后执行的会在上面)
+sed -i '1i src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 
 # # 追加 AdGuardHome 的第三方源
 # echo 'src-git luci_adguardhome https://github.com/sirpdboy/luci-app-adguardhome.git' >> feeds.conf.default
@@ -13,7 +13,7 @@ echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;ma
 rm -rf package/network/utils/fullconenat-nft/ || true
 
 # 安装 TurboACC（不启用 SFE）
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh \
-  && bash add_turboacc.sh --no-sfe
+# 参考 https://github.com/chenmozhijin/turboacc/tree/luci
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 
 
