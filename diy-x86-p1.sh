40074da1d69b11861742caee8df3d6c31b8f3b5b
tigzzr <root@Codes.aio>
2026-09-02 10:58:03 +0800

    新版本

--- diy-x86-p1.sh
+++ diy-x86-p1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# X86 (master) - feeds 定制
+# X86 (master) - Passwall2 feeds
+# 官方：packages = 内核/依赖，passwall2 = luci-app-passwall2
+# 1i 插在第 1 行前；后执行的会在最上面
 
-# 在顶部插入 passwall (先执行的会在下面，后执行的会在上面)
-sed -i '1i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '1i src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
--- diy-x86-p2.sh
+++ diy-x86-p2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# X86 (master) - 默认主题与 IP 定制
+# X86 (master) - 默认主题与 IP（与手动编译文档一致）
 
 # 修改默认主题为 argon（luci-light / luci-nginx 集合）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile 2>/dev/null || true
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile 2>/dev/null || true
 
-# 修改默认 IP 地址为 192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 2>/dev/null || true
+# 修改默认 IP 地址为 192.168.10.3
+sed -i 's/192.168.1.1/192.168.10.4/g' package/base-files/files/bin/config_generate 2>/dev/null || true
